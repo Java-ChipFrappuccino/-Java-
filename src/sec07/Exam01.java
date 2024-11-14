9f9ec06713557e92ccde0539aa9f90d27fa6fb90
Joon <pss21242124@gmail.com>
2024-11-14 23:44:06 +0900

    24.11.14

--- src/sec07/Exam01.java
+++ src/sec07/Exam01.java
@@ -1,30 +1,37 @@
 package sec07;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+
+import static java.util.Collections.swap;
 
 // 1. 선택정렬
 public class Exam01 {
     public static void main(String[] args) {
         Exam01 exam = new Exam01();
 
-        List<Integer> arr1 = new ArrayList<>();
-        List<Integer> arr2 = new ArrayList<>();
-
         Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        for (int i = 0; i < num1; i++) {
-            arr1.add(sc.nextInt());
-        }
-        int num2 = sc.nextInt();
-        for (int i = 0; i < num2; i++) {
-            arr2.add(sc.nextInt());
-        }
-        exam.solution(arr1,arr2);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for (int i = 0; i < num; i++) arr[i] = sc.nextInt();
+
+        exam.solution(arr);
     }
 
-    public void solution(List<Integer> arr1, List<Integer> arr2) {
-
+    public void solution(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int idx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[idx] > arr[j]) {
+                    idx = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
